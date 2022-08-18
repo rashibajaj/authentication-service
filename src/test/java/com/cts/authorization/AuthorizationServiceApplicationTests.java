58da74ee0989028216eda20d8ac995a76c42815a
Rashi Bajaj <bajajrashi12@gmail.com>
2022-08-18 20:55:40 +0530

    code commit

---
+++ src/test/java/com/cts/authorization/AuthorizationServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cts.authorization;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cts.authorization.service.UserServiceImpl;
+
+@SpringBootTest
+class AuthorizationServiceApplicationTests {
+
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+
+
+}
